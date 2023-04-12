1e2f555259805edd9219cee86e113c5bed51b917
TimofeyTst <yarik.mist@gmail.com>
2023-04-12 21:21:36 +0300

    v 1.0 Chart now should be

--- frontend/src/components/Chart/ChartNow.jsx
+++ frontend/src/components/Chart/ChartNow.jsx
@@ -1,23 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Cell } from 'recharts';
+import { fetchData } from '../../api/data';
 import '../../styles/Chart.css'
 
 const ChartNow = () => {
   const [data, setData] = useState([]);
 
-  const fetchData = () => {
-    fetch('http://127.0.0.1:5000/getCurrentValue')
-      .then(response => response.json())
-      .then(responseData => {
-        const newData = [...data, { time: new Date().toLocaleTimeString(), people: responseData }];
-        setData(newData);
-      })
-      .catch(error => console.log(error));
+  const updateData = async () => {
+    const newPeople = await fetchData();
+    const newData = [...data.slice(-14), { time: new Date().toLocaleTimeString(), people: newPeople }];
+    setData(newData);
+    localStorage.setItem('data', JSON.stringify(newData));
   };
 
   useEffect(() => {
+    const savedData = JSON.parse(localStorage.getItem('data'));
+    if (savedData) {
+      setData(savedData);
+    } else {
+      const fetchDataAndUpdate = async () => {
+        const newPeople = await fetchData();
+        const initialData = [{ time: new Date().toLocaleTimeString(), people: newPeople }];
+        setData(initialData);
+        localStorage.setItem('data', JSON.stringify(initialData));
+      };
+      fetchDataAndUpdate();
+    }
+  }, []);
+
+  useEffect(() => {
     const intervalId = setInterval(() => {
-      fetchData();
+      updateData();
     }, 2000);
 
     return () => {
@@ -25,22 +38,42 @@ const ChartNow = () => {
     };
   }, [data]);
 
+  const getFill = (people) => {
+    if (people < 5) {
+      return '#21d927';
+    } else if (people >= 5 && people <= 15) {
+      return '#f2b007';
+    } else {
+      return '#e82e2e';
+    }
+  };
+
   return (
     <div className='chart-box'>
-            <h1 className='header_text'>Загруженность столовой</h1>
-    <BarChart width={800} height={400} data={data} margin={{
+      <div className='chart-text'>Загруженность столовой в последние 30 секунд</div>
+      <BarChart 
+        width={500}
+        height={300}
+        data={data}
+        margin={{
         top: 5,
         right: 30,
         left: 20,
         bottom: 5,
         }}
-        barSize={20}>
-      <XAxis dataKey="time" />
-      <YAxis />
-      <CartesianGrid stroke={"#eee"} strokeDasharray="5 5" />
-      <Bar dataKey="people" fill="orange" background={{ fill: '#eee' }}/>
-      <Tooltip />
-    </BarChart>
+        barSize={20}
+        barCategoryGap={1}
+      >
+        <XAxis dataKey="time" />
+        <YAxis />
+        <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+        <Bar dataKey="people" fill="orange" isAnimationActive={false}>
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={getFill(entry.people, index)} />
+          ))}
+        </Bar>
+        <Tooltip />
+      </BarChart>
     </div>
   );
 };
